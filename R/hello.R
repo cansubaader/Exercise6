586af56c3c9f7f752b05955b9c42ba0d633008af
Cansu <c.baader@hotmail.de>
2023-07-23 21:28:30 +0200

    Package

---
+++ R/hello.R
@@ -0,0 +1,54 @@
+#' @title A function that shows filtered vegan recipes
+#' @param sweets A named list of recipes.
+#' @return  returns vegan recipes out of all available recipes
+#' @export
+#'
+#' @example
+#' vegan_recipes(sweets)
+
+
+library(dplyr)
+library(tidyverse)
+
+
+sweets <- c(brownie = (c("eggs", "flour", "sugar", "dark chocolate", "cacao powder", "butter", "salt", "vanilla")),
+            vegan_brownie = (c("butternut pumkin", "melted chocolate", "coconut oil")),
+            filled_dates = (c("dates", "nut butter")),
+            black_bean_brownie = (c("eggs", "black beans", "melted chocolate", "cane sugar", "cacao powder", "baking powder", "coconut oil")),
+            acai_bowl = (c("frozen acai", "apple juice", "bananas", "strawberries")),
+            muffins = (c("flour", "sugar", "milk", "vanilla", "baking powder", "butter", "dried cherries"))
+)
+#Lösung aus Suche, Funktion 1: rausfiltern, welche Rezepte vegan sind
+vegan_recipes_filter <- function(sweets) {
+  sweets_tbl <- enframe(sweets, name = "treat", value = "ingredients")
+  vegan_recipes <- sweets %>%
+    filter(!grepl("eggs", ingredients, ignore.case = TRUE) &
+             !grepl("butter", ingredients, ignore.case = TRUE) &
+             !grepl("milk", ingredients, ignore.case = TRUE))
+  return(vegan_recipes)
+}
+
+
+#' @title A function that shows the needed ingredients for the choosen recipe
+#' @param sweets A named list of recipes.
+#' @return  returns the ingredients
+#' @export
+#'
+#' @example
+#' needed_ingredients("filled_dates1", sweets)
+
+
+needed_ingredients <- function(name_of_recipe, sweets) {
+  # Convert input to lowercase for case-insensitive search
+  name_of_recipe <- tolower(name_of_recipe)
+
+  # Check if the recipe name exists
+  if (name_of_recipe %in% names(sweets)) {
+    return(sweets[[name_of_recipe]])
+  } else {
+    return(NULL)
+  }
+}
+
+
+
